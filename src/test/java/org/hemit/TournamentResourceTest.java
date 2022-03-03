466e1d8839054f1eac6df2e1b441f433575f98de
TheFirehawk33 <lotfi_rais@outlook.fr>
2022-03-03 16:36:06 +0100

    Mise à jour

--- src/test/java/org/hemit/TournamentResourceTest.java
+++ src/test/java/org/hemit/TournamentResourceTest.java
@@ -3,7 +3,9 @@ package org.hemit;
 import io.quarkus.test.junit.QuarkusTest;
 import org.hemit.model.CreateResponse;
 import org.hemit.model.Tournament;
+import org.hemit.services.TournamentRepository;
 import org.hemit.utils.TournamentUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -12,6 +14,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @QuarkusTest
 public class TournamentResourceTest {
+
+    @BeforeEach
+    public void setup()
+    {
+        TournamentRepository.cleanLocalBase();
+    }
 
     @Test
     public void create_should_returnAnId() {
@@ -23,7 +31,7 @@ public class TournamentResourceTest {
     }
 
     @Test
-    public void create_should_returnBadRequest() {
+    public void create_withoutName_should_returnBadRequest() {
 
         StatusAndContent<CreateResponse> response = TournamentUtils.createTournament(null);
 
@@ -32,11 +40,21 @@ public class TournamentResourceTest {
     }
 
     @Test
+    public void create_withEmptyName_should_returnBadRequest() {
+
+        StatusAndContent<CreateResponse> response = TournamentUtils.createTournament("");
+
+        assertThat(response.statusCode, is(400));
+        assertThat(response.content.id, is(null));
+    }
+
+    @Test
     public void create_should_returnAlreadyExist() {
 
+        TournamentUtils.createTournament("Unreal Tournament");
         StatusAndContent<CreateResponse> response = TournamentUtils.createTournament("Unreal Tournament");
 
-        assertThat(response.statusCode, is(302));
+        assertThat(response.statusCode, is(400));
         assertThat(response.content.id, is(notNullValue()));
     }
 
@@ -52,8 +70,19 @@ public class TournamentResourceTest {
     }
 
     @Test
-    public void startTournament_should_delete_TournamentToCreate() {
-        //ptdr je sais pas quoi faire en fait
+    public void get_tournament_with_no_id() {
+        StatusAndContent<Tournament> getResponse = TournamentUtils.getTournamentById(null);
+
+        assertThat(getResponse.statusCode, is(404));
+        assertThat(getResponse.content.name, is(null));
+    }
+
+    @Test
+    public void get_tournament_false_id_should_return_404(){
+        StatusAndContent<Tournament> getResponse = TournamentUtils.getTournamentById("AAAAA");
+
+        assertThat(getResponse.statusCode, is(404));
+        assertThat(getResponse.content.name, is(null));
     }
 }
 
